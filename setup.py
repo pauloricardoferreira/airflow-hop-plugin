7f2b1d93e53f687e887980847d5c2abaadc30194
Paulo Ricardo Ferreira <paulo.ricardo@agroterenas.com.br>
2026-03-03 10:47:06 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='airflow-hop-plugin',
-    version='0.1.0b51',
+    version='0.1.0b52',
     license='Apache 2.0',
     author='Damavis',
     author_email='info@damavis.com',
